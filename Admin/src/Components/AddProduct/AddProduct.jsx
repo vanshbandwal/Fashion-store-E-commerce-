bbad0f69ea92000714b3505c7b79d12b1f8d1357
vanshbandwal <bandwalvansh@gmail.com>
2025-07-07 10:38:07 -0700

    Update AddProduct.jsx

--- Admin/src/Components/AddProduct/AddProduct.jsx
+++ Admin/src/Components/AddProduct/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image)
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://fashion-store-e-commerce-one.vercel.app/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -37,7 +37,7 @@ const AddProduct = () => {
         if(responseData.success){
             product.image = responseData.image_url
             console.log(product)
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://fashion-store-e-commerce-one.vercel.app/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'Application/json',
